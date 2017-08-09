f7d6c51bd23e17d490d803b208b922dbaea747d5
George Higbie <gehigbie@gmail.com>
2017-08-09 23:54:45 +0000

    Remove dupe files

--- main.js
+++
@@ -1,6 +0,0 @@
-/* global angular */
-var app = angular.module('minmax', []);
-
-app.controller('MinMaxCtrl', function($scope){
-   $scope.formModel = {}; 
-});
--- modController.js
+++
@@ -1,9 +0,0 @@
-/*global angular*/
-
-var minmax = angular.module('minmax', []);
-
-minmax.controller('MinMax', function($scope){
-        $scope.formModel = {};
-    }
-
-);
