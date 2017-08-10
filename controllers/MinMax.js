f7580255abfbe7bad20377f9f2fdff09cea9f7f2
George Higbie <gehigbie@gmail.com>
2017-08-10 14:58:40 +0000

    Add ng-submit directive to form

--- controllers/MinMax.js
+++ controllers/MinMax.js
@@ -1,12 +1,18 @@
 /*global angular */
 
 (function(){
-    var MinMaxCtrl = function($scope){
+    var MinMaxCtrl = function($scope, $http){
         $scope.formModel = {};
         
         $scope.onSubmit = function(){
             console.log(`Hey, I'm submitted!!!`);
             console.log($scope.formModel);
+            $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
+                .success(function(data){
+                    console.log(": )"); 
+                }).error(function(data){
+                    console.log(": (");
+                });
         };
     };
     
