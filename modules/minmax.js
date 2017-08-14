8769b5be30c0e28a06d8f4e79779872d8f7d48b6
George Higbie <gehigbie@gmail.com>
2017-08-14 13:46:59 +0000

    Add some validation directives

--- modules/minmax.js
+++ modules/minmax.js
@@ -4,5 +4,13 @@
     var minmax = angular.module('minmax', [
         'jcs-autoValidate',
         'ngRoute']);
+        
+    minmax.run(function(defaultErrorMessageResolver){
+        defaultErrorMessageResolver.getErrorMessages().then(function (errorMessages){
+            errorMessages['tooYoung'] = 'You must be at least {0} years old to use this site';
+            errorMessages['toOld'] = 'You must be max {0} years old to use this site';
+            errorMessages['badUsername'] = 'The username can only contain numbers and letters and _';
+        });
+    });
     
 }());    
