ee75f895e4323f3724d0b7fc6f004f1f6dd4afdd
George Higbie <gehigbie@gmail.com>
2017-08-14 14:38:23 +0000

    Add ladda directives to shell

--- controllers/MinMax.js
+++ controllers/MinMax.js
@@ -3,14 +3,18 @@
 (function(){
     var MinMaxCtrl = function($scope, $http){
         $scope.formModel = {};
+        $scope.submitting = false;
         
         $scope.onSubmit = function(){
+            $scope.submitting = true;
             console.log(`Hey, I'm submitted!!!`);
             console.log($scope.formModel);
             $http.post('https://minmax-server.herokuapp.com/register/', $scope.formModel)
                 .success(function(data){
+                    $scope.submitting = false;
                     console.log(": )"); 
                 }).error(function(data){
+                    $scope.submitting = false;
                     console.log(": (");
                 });
         };
